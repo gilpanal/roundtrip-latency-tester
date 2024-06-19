f5b86af8dd3755870ec37932b724ff725a75de7e
fan-droide <jose.gilpanal@telecom-paris.fr>
2024-06-19 16:40:12 +0200

    code refactoring and fix problem for Chrome when restarting test getting wrong value

--- src/main.js
+++ src/main.js
@@ -1,16 +1,30 @@
 import { TestLatency } from "./testlatency";
+
 function main() {
     if (!window.AudioWorkletNode) {
         alert("This experiment requires AudioWorklet, please try this on Firefox beta 76 and more recent");
         return;
     }
+    const inputProcessing = document.location.search.indexOf("input-processing") != -1;
+
+    console.log("Input processing: ", inputProcessing)
+
+    const constraints = inputProcessing ? {
+        audio: {
+            // disabling this because we're intentionally trying to echo 
+            echoCancellation: false,
+            noiseSupression: true,
+            autoGainControl: true
+        }
+    } : {
+        audio: {
+            echoCancellation: false,
+            noiseSupression: false,
+            autoGainControl: false
+        }
+    }
     
-    TestLatency.initialize()
-    $ = document.querySelectorAll.bind(document);
-    $("button#start")[0].onclick = TestLatency.startTest;
-    $("button#stop")[0].onclick = TestLatency.stopTest;
-    $("button#stop")[0].disabled = true;  
-    
-    //threshold = $("input[type=range]")[0].value; 
+    TestLatency.initialize(constraints)
 }
-main();
+
+main()
--- src/testlatency.js
+++ src/testlatency.js
@@ -1,104 +1,82 @@
 import MeasureProcessor from 'worklet:./processor.js'
+
 const safariVersionIndex = navigator.userAgent.indexOf('Version/')
 const versionString = navigator.userAgent.substring(safariVersionIndex + 8)
 const safariVersion = parseFloat(versionString)
 const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
 
-$ = document.querySelectorAll.bind(document);
-
 export class TestLatency {
 
     ac = null
 
-    analyser = null // not necessary
+    worklet_node = null
 
-    initialSetup = false
+    btnstart = null
 
-    inputProcessing = false
+    btnstop = null
 
-    stop = true // not necessary
+    static async initialize(constraints) {
 
-    static initialize() {
-        TestLatency.ac = new AudioContext();
-    
-        TestLatency.analyser = new AnalyserNode(TestLatency.ac);
-        TestLatency.analyser.fftSize = 2048;
-            
-        //var initialSetup = false;
-    
-        TestLatency.inputProcessing = document.location.search.indexOf("input-processing") != -1;
-        
-        console.log("Input processing: ", TestLatency.inputProcessing);
-    }
-    static async startTest() {       
-        
-        $("button#stop")[0].disabled = false;
-        $("button#start")[0].disabled = true;
-        TestLatency.stop = false;
-        await TestLatency.ac.resume();
+        TestLatency.buttonHandlers()
+
         try {
-            //await TestLatency.ac.resume();
-            if (!TestLatency.initialSetup) {
-                await TestLatency.ac.audioWorklet.addModule(MeasureProcessor);
-                var constraints = TestLatency.inputProcessing ? {
-                    audio: {
-                        // disabling this because we're intentionally trying to echo 
-                        echoCancellation: false,
-                        noiseSupression: true,
-                        autoGainControl: true
-                    }
-                } : {
-                    audio: {
-                        echoCancellation: false,
-                        noiseSupression: false,
-                        autoGainControl: false
-                    }
-                };
-                let stream = await navigator.mediaDevices.getUserMedia(constraints);
+            
+            let stream = await navigator.mediaDevices.getUserMedia(constraints)
 
-                var mic_source = TestLatency.ac.createMediaStreamSource(stream);
-                var worklet_node = new AudioWorkletNode(TestLatency.ac, 'measure-processor', { outputChannelCount: [1] });
-                worklet_node.channelCount = 1;
+            TestLatency.ac = new AudioContext()
+            
+            await TestLatency.ac.audioWorklet.addModule(MeasureProcessor)
 
-                // For Safari 16 and above when using echocancellation to false
-                // the input is dramatically reduced
-                let defaultGain = 1
-                if (isSafari && safariVersion > 16) {
-                    defaultGain = 50
-                }
-                const gainNode = TestLatency.ac.createGain()
-                gainNode.gain.value = defaultGain
+            var mic_source = TestLatency.ac.createMediaStreamSource(stream)
 
-                mic_source.connect(gainNode)
+            TestLatency.worklet_node = new AudioWorkletNode(TestLatency.ac, 'measure-processor', { outputChannelCount: [1] });
+            TestLatency.worklet_node.channelCount = 1;
 
-                gainNode.connect(worklet_node)
-                gainNode.connect(TestLatency.analyser)
-
-                worklet_node.connect(TestLatency.ac.destination)
-
-                //worklet_node.port.postMessage({ threshold: $("input")[0].value });
-
-                worklet_node.port.onmessage = function (e) {
-                    $("#latency")[0].innerText = (e.data.latency * 1000) + "ms"
-                    $("#computed")[0].innerText = (TestLatency.ac.outputLatency * 1000) + "ms"
-                }
-
-                // $("input[type=range]")[0].oninput = (e) => {
-                //     threshold = e.target.value;
-                //     worklet_node.port.postMessage({ threshold: e.target.value });
-                // };
-                TestLatency.initialSetup = true;
+            // For Safari 16 and above when using echocancellation to false
+            // the input is dramatically reduced
+            let defaultGain = 1
+            if (isSafari && safariVersion > 16) {
+                defaultGain = 50
             }
+            const gainNode = TestLatency.ac.createGain()
+            gainNode.gain.value = defaultGain
+
+            mic_source.connect(gainNode)
+
+            gainNode.connect(TestLatency.worklet_node)            
+
+            TestLatency.worklet_node.port.onmessage = function (e) {
+                TestLatency.displayResults(e)
+            }
+
         } catch (e) {
             console.log(e)
         }
     }
 
-    static stopTest() {
-        TestLatency.ac.suspend();
-        $("button#stop")[0].disabled = true;
-        $("button#start")[0].disabled = false;
-        TestLatency.stop = true;
+    static buttonHandlers() {
+        TestLatency.btnstart = document.getElementById('btn-start')
+        TestLatency.btnstop = document.getElementById('btn-stop')
+        TestLatency.btnstart.onclick = TestLatency.startTest;
+        TestLatency.btnstop.onclick = TestLatency.stopTest;
+        TestLatency.btnstop.disabled = true;
+    }
+
+    static displayResults(e){
+        document.getElementById('roundtriplatency-val').innerText = (e.data.latency * 1000) + "ms"
+        document.getElementById('outputlatency-val').innerText = (TestLatency.ac.outputLatency * 1000) + "ms"
+    }
+
+    static async startTest() {
+        TestLatency.worklet_node.connect(TestLatency.ac.destination)        
+        TestLatency.btnstop.disabled = false
+        TestLatency.btnstart.disabled = true
+    }
+
+    static async stopTest() {
+        TestLatency.worklet_node.disconnect(TestLatency.ac.destination)
+        TestLatency.btnstop.disabled = true
+        TestLatency.btnstart.disabled = false
     }
 
 }
